67c4bc115efeebf720cde181224336379cbdd181
jayjun123 <fire8803@gmail.com>
2025-12-15 13:52:52 +0900

    ?쒓났? ?뺤궛 ?꾩옣異붽? ?앹뾽 Autocomplete ?쒕∼?ㅼ슫 z-index ?섏젙 諛??쒓났? 留ㅼ묶 濡쒖쭅 媛쒖꽑

--- src/pages/TeamSettlement.jsx
+++ src/pages/TeamSettlement.jsx
@@ -6784,26 +6784,32 @@ const TeamSettlement = () => {
         maxWidth="sm"
         fullWidth
         container={() => document.body}
-        style={{ zIndex: 999999 }}
+        style={{ zIndex: 9999999 }}
         PaperProps={{
           sx: { 
             bgcolor: '#1a1d21', 
             color: '#fff',
-            zIndex: 999999,
+            zIndex: 9999999,
             position: 'relative'
           },
-          style: { zIndex: 999999 }
+          style: { zIndex: 9999999 }
+        }}
+        BackdropProps={{
+          sx: {
+            bgcolor: 'rgba(0, 0, 0, 0.8)',
+            zIndex: 9999998
+          }
         }}
         sx={{
-          zIndex: 999999,
+          zIndex: 9999999,
           '& .MuiDialog-container': {
-            zIndex: 999999
+            zIndex: 9999999
           },
           '& .MuiBackdrop-root': {
-            zIndex: 999998
+            zIndex: 9999998
           },
           '& .MuiDialog-root': {
-            zIndex: 999999
+            zIndex: 9999999
           }
         }}
       >
@@ -6843,6 +6849,20 @@ const TeamSettlement = () => {
                 option.toLowerCase().includes(inputValue.toLowerCase())
               );
               return filtered;
+            }}
+            componentsProps={{
+              popper: {
+                style: { zIndex: 9999999 },
+                sx: { zIndex: '9999999 !important' }
+              },
+              paper: {
+                style: { zIndex: 9999999 },
+                sx: { 
+                  zIndex: '9999999 !important',
+                  bgcolor: '#1a1d21',
+                  color: '#fff'
+                }
+              }
             }}
             renderInput={(params) => (
               <TextField
@@ -6886,12 +6906,16 @@ const TeamSettlement = () => {
               '& .MuiAutocomplete-clearIndicator': {
                 color: '#fff'
               },
+              '& .MuiAutocomplete-popper': {
+                zIndex: '9999999 !important'
+              },
               '& .MuiAutocomplete-paper': {
-                bgcolor: '#2d2d2d',
-                color: '#fff'
+                bgcolor: '#1a1d21',
+                color: '#fff',
+                zIndex: '9999999 !important'
               },
               '& .MuiAutocomplete-listbox': {
-                bgcolor: '#2d2d2d'
+                bgcolor: '#1a1d21'
               },
               '& .MuiAutocomplete-option': {
                 color: '#fff',
